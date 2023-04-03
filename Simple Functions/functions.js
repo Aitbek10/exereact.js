2876c3c34d1ee1f0771afc3dfcd164a4438a7930
aitbek10 <egemberdievajtbek495@gmail.com>
2023-04-03 10:35:25 +0600

    github

---
+++ Simple Functions/functions.js
@@ -0,0 +1,30 @@
+// Возвращает число
+function returnNumber() {
+    return 42;
+  }
+  
+  // Возвращает строку
+  function returnString() {
+    return "Привет, мир!";
+  }
+  
+  // Возвращает логическое значение true
+  function returnTrue() {
+    return true;
+  }
+  
+  // Возвращает логическое значение false
+  function returnFalse() {
+    return false;
+  }
+  
+  // Возвращает пустой массив
+  function returnArray() {
+    return [];
+  }
+  
+  // Возвращает объект с свойством "имя", установленным на "Иван"
+  function returnObject() {
+    return { имя: "Иван" };
+  }
+  
